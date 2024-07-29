e4699e1b18a0290d426169482cf219ad133c4b48
michelleg0 <michellegue8@gmail.com>
2024-07-29 16:42:21 -0700

    adding

---
+++ script.js
@@ -0,0 +1,18 @@
+// Function to simulate sending a message
+function sendMessage() {
+    // Get the message input value
+    const messageInput = document.getElementById('message');
+    const message = messageInput.value;
+
+    // Check if the input is not empty
+    if (message) {
+        // For now, just show an alert with the message
+        alert('Message sent: ' + message);
+
+        // Clear the input
+        messageInput.value = '';
+    } else {
+        // Show an alert if the input is empty
+        alert('Please enter a message.');
+    }
+}
